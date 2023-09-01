fa9df910f87bcf88479c0c1596aa4c279539b4b7
Usman Digitaldev <usmanaslamm138@gmail.com>
2023-09-01 19:43:31 +0500

    Completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,12 +22,74 @@ pageTurnBtn.forEach((el, index) => {
 })
 
 //contact me button when click
+const pages = document.querySelectorAll('.book-page.page-right');
+const contactMeBtn = document.querySelector('.btn.contact-me');
+
+contactMeBtn.onclick = () => {
+    pages.forEach((pages, index) => {
+        setTimeout(() => {
+            pages.classList.add('turn')
+            setTimeout(() => {
+                pages.style.zIndex = 20 + index;
+            }, 500)
+        }, (index + 1) * 200 + 100)
+    })
+}
 
 //create reverse index function
+let totalpages = pages.length;
+let pageNumber = 0;
 
+function reverseIndex() {
+    pageNumber--
+    if (pageNumber < 0) {
+        pageNumber = totalpages - 1;
+    }
+}
 //back profile button when click
+const backProfileBtn = document.querySelector(".back-profile")
+
+backProfileBtn.onclick = () => {
+    pages.forEach((_, index) => {
+        setTimeout(() => {
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+            setTimeout(() => {
+                reverseIndex()
+                pages[pageNumber].style.zIndex = 10 + index;
+            }, 500)
+        }, (index + 1) * 200 + 100)
+    })
+}
+
 
 //opening animation
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
 //opening animation (cover right animation)
+setTimeout(() => {
+    coverRight.classList.add('turn')
+}, 2100);
+
+setTimeout(() => {
+    coverRight.style.zIndex = -1
+}, 2800);
 //opening animation (page left or profile page animation)
+setTimeout(() => {
+    pageLeft.style.zIndex = 20;
+}, 3200)
+
+pages.forEach((_, index) => {
+    setTimeout(() => {
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+        setTimeout(() => {
+            reverseIndex()
+            pages[pageNumber].style.zIndex = 10 + index;
+        }, 500)
+    }, (index + 1) * 200 + 2100)
+})
+
+
 //opening animation (all page right animation)
